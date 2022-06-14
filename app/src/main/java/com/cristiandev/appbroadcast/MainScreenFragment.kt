e25989a00595f42342863375be43e93bf40fc5a7
Cristian Ramirez <cristiancrc97@hotmail.com>
2022-06-14 15:46:46 -0500

    fragment disegned using constraint layout

--- app/src/main/java/com/cristiandev/appbroadcast/MainScreenFragment.kt
+++ app/src/main/java/com/cristiandev/appbroadcast/MainScreenFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainScreen.newInstance] factory method to
+ * Use the [MainScreenFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainScreen : Fragment() {
+class MainScreenFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -44,12 +44,12 @@ class MainScreen : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainScreen.
+         * @return A new instance of fragment MainScreenFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainScreen().apply {
+            MainScreenFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
